4c226c2bcd762c3f4d9ff2430c1b944c024fcb60
Gabifran Coelho <116119431+GabifranCoelho@users.noreply.github.com>
2025-11-02 19:31:29 -0300

    fix: ajusts permissions gpt

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "pr-656003592068-tfstate-us-east-2"
+    key            = "iac/terraform.tfstate"   # caminho dentro do bucket (pode trocar)
+    region         = "us-east-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
